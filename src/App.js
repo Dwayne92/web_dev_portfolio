aef998926d42240b8a5ca180d08062714fe38f99
Oduaine Miller <oduainemiller@Oduaines-MacBook-Air.local>
2022-06-29 23:21:30 -0400

    major changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import About from './routes/About'
 import Selfie from './routes/Selfie'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick"
 // const getActivity = () => {
 //   axios.get('https://www.boredapi.com/api/activity').then((response) => {
 //     setActivity(response.data.activity)
@@ -24,6 +27,21 @@ import Selfie from './routes/Selfie'
 
 export default function App() {
 
+  const sliderContent = [{image: 'map.png'},{image: 'grecipe.png'}, {image: 'am.png'}, {image: 'rm.png'}]
+
+const settings = {
+  dots: false,
+  fade: false,
+  infinite: true,
+  speed: 700,
+  slideToShow: 1,
+  className: "slides",
+  autoplay: true,
+  speed: 2500,
+    autoplaySpeed: 3000,
+    cssEase: "linear"
+}
+
   const [apodPic,setapodPic] = useState('')
   const [activity,setActivity] = useState('')
   const getAPOD = () => {
@@ -37,42 +55,70 @@ export default function App() {
     getAPOD()
   }, [])
 
+
+  // <div className='shotcontainer'>
+  // <img src='IMG_5031.jpg' className='headshot' />
+  // </div>
   return (
 
     <div className='container'>
-    <Router>
     <nav>
-      <Link to='/' className='l'>Home</Link>
-      <Link to='/about' className='l'>About</Link>
-      <div className='shotcontainer'>
-      <img src='IMG_5031.jpg' className='headshot' />
-      </div>
+
 
     </nav>
-     <Routes>
-        <Route path='/' element={<Home />} />
-       <Route path='/about' element={<Test />} />
-
-     </Routes>
-    </Router>
 
 
-   <h1>Modern<br/>Web Development</h1>
-   <h5>Full-Stack Developer</h5>
+
+
    <div className='maincont'>
-   <div className='techicons'>
-   <img src='html5.png' className='tekicons' />
-   <img src='css3.png' className='tekicons' />
-   <img src='javascript.png' className='tekicons' />
-   <img src='bootstrap.png' className='tekicons' />
-   <img src='react-icon.png' className='tekicons' />
-   <img src='nodejs.png' className='tekicons' />
-   <img src='expressjs.png' className='tekicons' />
-   <img src='mongodb.png' className='tekicons' />
-   <img src='python-vertical.png' className='tekicons' />
-   <img src='django.png' className='tekicons' />
+   <div className='firstsec'>
+   <div className='landingimgcnt'>
+   <img className='landingimg' src='balloons.png' />
    </div>
-   <h6>I integrate 3rd party APIs</h6>
+   <div className='toprestdiv'>
+   <div className='title-div'>
+   <h1 className='title'>Modern<br/>Web Development</h1>
+   </div>
+   <h5 className='title'>Full-Stack Developer</h5>
+    <div className='techicons'>
+      <img src='html5.png' className='tekicons' />
+      <img src='css3.png' className='tekicons' />
+      <img src='javascript.png' className='tekicons' />
+      <img src='bootstrap.png' className='tekicons' />
+      <img src='react-icon.png' className='tekicons' />
+      <img src='nodejs.png' className='tekicons' />
+      <img src='expressjs.png' className='tekicons' />
+      <img src='mongodb.png' className='tekicons' />
+      <img src='python-vertical.png' className='tekicons' />
+      <img src='django.png' className='tekicons' />
+   </div>
+   </div>
+   </div>
+   <h4>I create sites for the web utilizing the latest technologies</h4>
+
+
+   </div>
+   <div className="slidersec">
+   <div className='sliderdiv'>
+   <Slider {...settings}>
+   {sliderContent.map((sites)=>{
+     return(
+       <div>
+       <img className="z-image" src={sites.image} />
+       </div>
+     )
+   })}
+   </Slider>
+   </div>
+   </div>
+   <div className="contactsec">
+   <div className="contactcontent">
+   <h1 className="contact-title">Contact</h1>
+   </div>
+   <div className="contactimgdiv">
+   <img className="contactimg" src="newphone.png" />
+   </div>
+   </div>
    <img src={apodPic} className='tnimg' />
    <div className='nasacaption'>
    <p className='nasades des'>This is an API from nasa, for an Astronomy picture
@@ -84,7 +130,6 @@ export default function App() {
    <p className='restdes des'>
    I also build APIs!
    </p>
-   </div>
    </div>
 
    </div>
@@ -106,3 +151,19 @@ function Test(){
     </div>
   )
 }
+
+// <Router>
+// <nav>
+//   <Link to='/' className='l'>Home</Link>
+//   <Link to='/about' className='l'>About</Link>
+//   <div className='shotcontainer'>
+//   <img src='IMG_5031.jpg' className='headshot' />
+//   </div>
+//
+// </nav>
+//  <Routes>
+//     <Route path='/' element={<Home />} />
+//    <Route path='/about' element={<Test />} />
+//
+//  </Routes>
+// </Router>
