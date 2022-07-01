853744e04356a2ad5087d242404987a24e07581f
Oduaine Miller <oduainemiller@Oduaines-MacBook-Air.local>
2022-07-01 06:04:44 -0400

    built out mid section

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
-import Navbar from './components/Navbar'
-import About from './routes/About'
-import Selfie from './routes/Selfie'
+// import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+// import Navbar from './components/Navbar'
+// import About from './routes/About'
+// import Selfie from './routes/Selfie'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick"
@@ -42,18 +42,7 @@ const settings = {
     cssEase: "linear"
 }
 
-  const [apodPic,setapodPic] = useState('')
-  const [activity,setActivity] = useState('')
-  const getAPOD = () => {
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=bDgsn2ZBWdTB347AgiOtJRdNXINesY9pBAo3dMxz').then((response) => {
-    setapodPic(response.data.url)
-    })
-  }
 
-
-  useEffect(() => {
-    getAPOD()
-  }, [])
 
 
   // <div className='shotcontainer'>
@@ -61,7 +50,7 @@ const settings = {
   // </div>
   return (
 
-    <div className='container'>
+  <div className='container'>
     <nav>
 
 
@@ -70,87 +59,115 @@ const settings = {
 
 
 
-   <div className='maincont'>
-   <div className='firstsec'>
-   <div className='landingimgcnt'>
-   <img className='landingimg' src='balloons.png' />
-   </div>
-   <div className='toprestdiv'>
-   <div className='title-div'>
-   <h1 className='title'>Modern<br/>Web Development</h1>
-   </div>
-   <h5 className='title'>Full-Stack Developer</h5>
-    <div className='techicons'>
-      <img src='html5.png' className='tekicons' />
-      <img src='css3.png' className='tekicons' />
-      <img src='javascript.png' className='tekicons' />
-      <img src='bootstrap.png' className='tekicons' />
-      <img src='react-icon.png' className='tekicons' />
-      <img src='nodejs.png' className='tekicons' />
-      <img src='expressjs.png' className='tekicons' />
-      <img src='mongodb.png' className='tekicons' />
-      <img src='python-vertical.png' className='tekicons' />
-      <img src='django.png' className='tekicons' />
-   </div>
-   </div>
-   </div>
-   <h4>I create sites for the web utilizing the latest technologies</h4>
+
+
+    <div className='firstsec'>
+      <div className='landingimgcnt'>
+        <img className='landingimg' src='balloons.png' />
+      </div>
+      <div className='toprestdiv'>
+        <div className='title-div'>
+          <h1 className='title'>Modern<br/>Web Development</h1>
+        </div>
+        <h5 className='title'>Full-Stack Developer</h5>
+        <div className='techicons'>
+          <img src='html5.png' className='tekicons' />
+          <img src='css3.png' className='tekicons' />
+          <img src='javascript.png' className='tekicons' />
+          <img src='bootstrap.png' className='tekicons' />
+          <img src='react-icon.png' className='tekicons' />
+          <img src='nodejs.png' className='tekicons' />
+          <img src='expressjs.png' className='tekicons' />
+          <img src='mongodb.png' className='tekicons' />
+          <img src='python-vertical.png' className='tekicons' />
+          <img src='django.png' className='tekicons' />
+        </div>
+      </div>
+    </div>
+      <div className="slidersec">
+      <div className="slidercontentdiv">
+        <div className="slidercolumnholder">
+        <u><h2 className="slidersectitle">Built and Designed</h2></u>
+        <p className="slidersec-content">These are a collection of projects that I designed and built using
+        various technologies such as HTML, CSS, Javascript, Typescript, React.js,
+        Node.js, SQL & NOSQL databases, Python, Django, Flask, etc. For more
+        information, contact me.</p>
+        </div>
+      </div>
+        <div className='sliderdiv'>
+          <Slider {...settings}>
+            {sliderContent.map((sites)=>{
+              return(
+                <div>
+                  <img className="z-image" src={sites.image} />
+                </div>
+                  )
+                  })}
+          </Slider>
+        </div>
+      </div>
+      <div className="contactsec">
+        <div className="contactcontent">
+          <h1 className="contact-title">Contact</h1>
+        </div>
+        <div className="contactimgdiv">
+          <img className="contactimg" src="nphone.png" />
+        </div>
 
 
    </div>
-   <div className="slidersec">
-   <div className='sliderdiv'>
-   <Slider {...settings}>
-   {sliderContent.map((sites)=>{
-     return(
-       <div>
-       <img className="z-image" src={sites.image} />
-       </div>
-     )
-   })}
-   </Slider>
-   </div>
-   </div>
-   <div className="contactsec">
-   <div className="contactcontent">
-   <h1 className="contact-title">Contact</h1>
-   </div>
-   <div className="contactimgdiv">
-   <img className="contactimg" src="newphone.png" />
-   </div>
-   </div>
-   <img src={apodPic} className='tnimg' />
-   <div className='nasacaption'>
-   <p className='nasades des'>This is an API from nasa, for an Astronomy picture
-   of the day Come back tomorrow for another picture!</p>
-   </div>
-   <h4>Here is an API that gives random activities</h4>
-
-   <div className='rest'>
-   <p className='restdes des'>
-   I also build APIs!
-   </p>
    </div>
 
-   </div>
+
 
 
   );
 }
-function Home(){
-  return(
-    <div>
-    <h1>Home</h1>
-    </div>
-  )
-}
-function Test(){
-  return (
-    <div>
-    <h1>Test</h1>
-    </div>
-  )
-}
+
+// const [apodPic,setapodPic] = useState('')
+// const [activity,setActivity] = useState('')
+// const getAPOD = () => {
+//   axios.get('https://api.nasa.gov/planetary/apod?api_key=bDgsn2ZBWdTB347AgiOtJRdNXINesY9pBAo3dMxz').then((response) => {
+//   setapodPic(response.data.url)
+//   })
+// }
+//
+//
+// useEffect(() => {
+//   getAPOD()
+// }, [])
+
+// function Home(){
+//   return(
+//     <div>
+//     <h1>Home</h1>
+//     </div>
+//   )
+// }
+// function Test(){
+//   return (
+//     <div>
+//     <h1>Test</h1>
+//     </div>
+//   )
+// }
+
+
+// <img src={apodPic} className='tnimg' />
+// <div className='nasacaption'>
+// <p className='nasades des'>This is an API from nasa, for an Astronomy picture
+// of the day Come back tomorrow for another picture!</p>
+// </div>
+// <h4>Here is an API that gives random activities</h4>
+//
+// <div className='rest'>
+// <p className='restdes des'>
+// I also build APIs!
+// </p>
+// </div>
+//
+// </div>
+
 
 // <Router>
 // <nav>
